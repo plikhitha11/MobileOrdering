ae6c2fd96d8e47ae3f60a22354672108fd7769b6
p likhitha <plikhitha11>
2024-08-27 12:29:52 +0530

    first commit

---
+++ select.js
@@ -0,0 +1,42 @@
+{
+    "mobiles"[
+      {
+        "name": "Galaxy S21",
+        "brand": "Samsung",
+        "price": 799,
+        "specifications": {
+          "display": "6.2 inches",
+          "processor": "Exynos 2100",
+          "ram": "8GB",
+          "battery": "4000mAh"
+        },
+        "image": "link_to_image_1"
+      },
+      {
+        "name": "iPhone 13",
+        "brand": "Apple",
+        "price": 999,
+        "specifications": {
+          "display": "6.1 inches",
+          "processor": "A15 Bionic",
+          "ram": "6GB",
+          "battery": "3240mAh"
+        },
+        "image": "link_to_image_2"
+      },
+      {
+        "name": "OnePlus 9",
+        "brand": "OnePlus",
+        "price": 729,
+        "specifications": {
+          "display": "6.55 inches",
+          "processor": "Snapdragon 888",
+          "ram": "8GB",
+          "battery": "4500mAh"
+        },
+        "image": "link_to_image_3"
+      }
+      // Add 6 more mobile objects here
+    ]
+  }
+  
